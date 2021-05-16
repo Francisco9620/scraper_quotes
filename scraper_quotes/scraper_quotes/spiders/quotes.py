fb6cfbe7598384fb2ee6e021af7e89ab8663b67b
Francisco R Espinosa <franciscoreg9620@gmail.com>
2021-05-16 16:40:58 -0400

    Primer commit del proyecto Ya se puede usar los spider ya se puede guardad en diferentes formatos

---
+++ scraper_quotes/scraper_quotes/spiders/quotes.py
@@ -0,0 +1,26 @@
+import scrapy
+#Titulos = //h1/a/text()
+#Citas = //span[@class="text" and @itemprop="text"]/text()
+#Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+
+
+class ScraperQuotes(scrapy.Spider):
+    name='quotes'
+    start_urls= [
+        'https://quotes.toscrape.com/page/1/'
+    ]
+
+    def parse(self, response):
+        title= response.xpath('//h1/a/text()').get()
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        
+        yield {
+            'title': title,
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+
+        }
+
+
+       
